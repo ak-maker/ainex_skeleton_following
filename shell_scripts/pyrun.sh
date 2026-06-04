345b95b55f56b3f81ed9052f7f0b428e607502d5
Scott Fukuda <sdf72@cornell.edu>
2026-06-04 16:35:19 -0400

    added stand.py demo script and updated pyrun script

--- shell_scripts/pyrun.sh
+++ shell_scripts/pyrun.sh
@@ -22,15 +22,16 @@ fi
 
 REL_PATH="${1##/}"
 SCRIPT_FULL="${ROS_SCRIPT_BASE}/${REL_PATH}"
+shift  # remove the script path, leaving any extra flags in $@
 
 if [[ ! -f "$SCRIPT_FULL" ]]; then
   echo "✗ File not found: ${SCRIPT_FULL}"
   exit 1
 fi
 
-echo "▶ pyrun  ${REL_PATH}"
+echo "▶ pyrun  ${REL_PATH} $@"
 echo ""
 
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/ros_ws/devel/setup.bash
-python3 "$SCRIPT_FULL"
+python3 "$SCRIPT_FULL" "$@"
